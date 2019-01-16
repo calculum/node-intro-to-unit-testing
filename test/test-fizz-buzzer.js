be392d289725b3b11374f80f783d2c8f5932c5c2
bo <clouculum@gmail.com>
2019-01-16 01:20:53 -0700

    update

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,22 +1,32 @@
 const expect = require('chai').expect;
 
-const fizzBuzzer = require('./fizzBuzzer');
+const fizzBuzzer = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function () {
     //narmal case
-    it('Please enter a number', function () {
+    it('Number entered.', function () {
         const normalCases = [
-            {num : 1},
-            {num : 0.5},
-            {num : 1.1},
+            {num : 1, expected : 1},
+            {num : 0.5, expected : 0.5},
+            {num : 1.1, expected : 1.1},
         ];
 
         normalCases.forEach(function(input) {
-            const answer = fizzBuzzer(num);
-            expect(answer).to.equal(input.num)
+            const answer = fizzBuzzer(input.num);
+            expect(answer).to.equal(input.expected)
         });
     });
 
-    //
-
-})
+    //edge case
+    it('Not a number detected.', function () {
+        const badInputs = [
+            ['$'],
+            ['L']
+        ];
+        badInputs.forEach(function(input) {
+            expect(function() {
+                fizzBuzzer(input[0]);
+            }).to.throw(Error);
+        });
+    });
+});
